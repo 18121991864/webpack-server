a15e8584979fb2a59de0a9486333cc114f4cb36b
tl <tl>
2025-06-24 18:39:17 +0800

    fix: 数据库链接

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import "./config/envLoader"; // !! 确保这是文件的第一行 !!
-import express from "express";
-
-const app = express();
+import express, { Request, Response, Application } from "express";
+import db from "./db";
+const app: Application = express();
+app.use(express.json());
 const port = 3000;
 
 app.get("/", (req, res) => {
@@ -9,6 +10,16 @@ app.get("/", (req, res) => {
     name: "zs",
     age: 18,
   });
+});
+// 创建一个新路由来获取所有用户
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const users = await db("users").select("id", "username", "email", "created_at");
+    res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 app.listen(port, () => {
